350dacfc78ccaf321eb5aa9d119f4f3854979583
Larry Motuzis <mlauris2006@yahoo.com>
2025-09-20 00:13:06 -0500

    add main.py file basic layout

---
+++ src/main.py
@@ -0,0 +1,15 @@
+"""
+Title: Finding Fermats Near Misses
+External input files required: 
+External files created: 
+Programmer: Laurynas Motuzis - mlauris2006@yaoo.com, Kaushalkumar Dineshbhai Sharma - Kdsharma1706@gmail.com
+Course/Section: Software Engineering - PSC 60500, Section 1
+Program Completion Date:
+Program Submission Date:
+
+
+Program Summary: 
+
+Resources Used:
+
+"""
